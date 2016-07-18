08aabb37e531bb2e9c5205ae99d7d9f6628e0d97
pyerraboina <pavan.yerraboina@teamaol.com>
2016-07-18 12:52:40 +0100

    first commit

---
+++ api/controllers/TesttableController.js
@@ -0,0 +1,11 @@
+/**
+ * TesttableController
+ *
+ * @description :: Server-side logic for managing testtables
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
